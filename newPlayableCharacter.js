615a7dd1765f6e038b81979d8be562e12e3cc5b0
milcab <milcarodriguez23@gmail.com>
2021-11-07 23:01:37 -0500

    web game part 6

--- newPlayableCharacter.js
+++ newPlayableCharacter.js
@@ -1,6 +1,7 @@
 function newPlayableCharacter(x, y) {
     const element = newImage('assets/green-character/static.gif')
     element.style.zIndex = 1;
+   
 
     function handleDirectionChange(direction) {
         if (direction === null) {
